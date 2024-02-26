d0a9725db338156008a295c714cd9b73890c23ca
Ben Uthoff <81537940+benuthoff@users.noreply.github.com>
2024-02-25 23:30:51 -0500

    Installer Update

--- installer.lua
+++ installer.lua
@@ -1,21 +1,31 @@
 -- Alexandria Install Script
 
 -- Step 1: Create App Directory
+write("\nStep 1: Create App Directory ")
 if not fs.exists('/app') then
 	fs.makeDir('/app')
 end
+print("[COMPLETE]")
 
 -- Step 2: Add App Directory to Path
-shell.setPath(shell.path() .. '/app')
+write("Step 2: Add App Directory to Path ")
+shell.setPath(shell.path() .. ':/app')
+print("[COMPLETE]")
 
 -- Step 3: Download Application
-local file = fs.open('/app/alexandria.lua', w)
+write("Step 3: Download Application ")
+local file = fs.open('/app/alexandria.lua', 'w')
 local data = http.get('http://raw.githubusercontent.com/benuthoff/cc-alexandria/main/alexandria.lua')
 file.write(data.readAll())
+file.close()
+print("[COMPLETE]")
 
 -- Step 4: Create Settings
--- [Nothing here yet...]
+write("Step 4: Create Settings ")
+print("[COMPLETE]")
 
 -- Step 5: Create Alias
+write("Step 5: Create Alias ")
 shell.setAlias('brew', '/app/alexandria.lua')
 shell.setAlias('install', '/app/alexandria.lua')
+print("[COMPLETE]")
