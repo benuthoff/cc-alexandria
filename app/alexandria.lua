a36cc106ec96116b736f393c7228310f9232c2e4
Ben Uthoff <81537940+benuthoff@users.noreply.github.com>
2024-02-26 09:51:42 -0500

    Alexandria Update

--- app/alexandria.lua
+++ app/alexandria.lua
@@ -7,7 +7,14 @@ local req = http.get('http://raw.githubusercontent.com/benuthoff/cc-alexandria/m
 if req == nil then
 	printError('[ERR] Could not download program "'..name..'".')
 else
+	if fs.exists('/app/'..name..'.lua') then
+		print('Updating program...')
+	else
+		print('Downloading program...')
+	end
 	local file = fs.open('/app/'..name..'.lua','w')
 	file.write(req.readAll())
 	file.close()
+	term.setTextColor(colors.lime)
+	print('[ Opperation Successful ]')
 end
