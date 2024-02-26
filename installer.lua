272d3e631d7b5537a0278a6d827c6c736fa1ba01
Ben Uthoff <81537940+benuthoff@users.noreply.github.com>
2024-02-26 00:04:09 -0500

    Update Installer

--- installer.lua
+++ installer.lua
@@ -1,18 +1,13 @@
 -- Alexandria Install Script
 
 -- Step 1: Create App Directory
-write("\nStep 1: Create App Directory ")
+write("Step 1: Create App Directory ")
 if not fs.exists('/app') then
 	fs.makeDir('/app')
 end
 print("[COMPLETE]")
 
--- Step 2: Add App Directory to Path
-write("Step 2: Add App Directory to Path ")
-shell.setPath(shell.path() .. ':/app')
-print("[COMPLETE]")
-
--- Step 3: Download Application
+-- Step 2: Download Application
 write("Step 3: Download Application ")
 local file = fs.open('/app/alexandria.lua', 'w')
 local data = http.get('http://raw.githubusercontent.com/benuthoff/cc-alexandria/main/app/alexandria.lua')
@@ -20,11 +15,12 @@ file.write(data.readAll())
 file.close()
 print("[COMPLETE]")
 
--- Step 4: Create Settings
-write("Step 4: Create Settings ")
-print("[COMPLETE]")
-
--- Step 5: Create Alias
-write("Step 5: Create Alias ")
+-- Step 3: Create Startup
+write("Step 4: Create Startup & Alias ")
+shell.setPath(shell.path() .. ':/app')
 shell.setAlias('brew', '/app/alexandria.lua')
+local file = fs.open('.startup.lua', 'a')
+file.writeLine("shell.setPath(shell.path() .. ':/app')")
+file.writeLine("shell.setAlias('brew', '/app/alexandria.lua')")
+file.close()
 print("[COMPLETE]")
